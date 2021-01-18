589d7d3eba5d752cefea4a29f709c6a705a7ba37
ZhuHongji <735765574@qq.com>
2021-01-18 22:34:43 +0800

    Add a new function. When the user use this app for the first time, the app will create a new List with nothing in it.

--- Checklists/Checklists/DataModel.swift
+++ Checklists/Checklists/DataModel.swift
@@ -21,6 +21,7 @@ class DataModel{
     init(){
         loadChecklists()
         registerDefaults()
+        handleFirstTime()
     }
     
     //MARK:- Data Saving
@@ -58,8 +59,23 @@ class DataModel{
     
     //初始化UserDefaults，解决重新安装app导致的UserDefaults初值为0的问题
     func registerDefaults(){
-        let dictionary = ["ChecklistIndex": -1]
+        let dictionary = ["ChecklistIndex": -1, "FirstTime": true] as [String: Any]
         UserDefaults.standard.register(defaults: dictionary)
+    }
+    
+    
+    func handleFirstTime(){
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+        
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            
+            indexOfSelectedChecklist = 0
+            userDefaults.set(false,forKey: "FirstTime")
+            userDefaults.synchronize()
+        }
     }
     
     
